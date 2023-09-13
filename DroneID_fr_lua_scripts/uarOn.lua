279c4c177d9a815f15f9b909422d86b55fbb1b0e
freedom2000 <31324055+f2knpw@users.noreply.github.com>
2023-09-13 22:19:11 +0200

    Update uarOn.lua

--- DroneID_fr_lua_scripts/uarOn.lua
+++ DroneID_fr_lua_scripts/uarOn.lua
@@ -1,7 +1,7 @@
 
 local function run()
   -- Called periodically while the Special Function switch is on
-  -- switch on serial port 1 : serialSetPower(port_nr, value)
+  -- switch on serial port 2 : serialSetPower(port_nr, value)
 	serialSetPower(1, 1)
 end
 
