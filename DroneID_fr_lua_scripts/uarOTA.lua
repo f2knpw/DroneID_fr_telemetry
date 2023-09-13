d5990fe717f94e3c778903dcef0aaf8f45118ebe
freedom2000 <31324055+f2knpw@users.noreply.github.com>
2023-09-13 22:19:46 +0200

    Update uarOTA.lua

--- DroneID_fr_lua_scripts/uarOTA.lua
+++ DroneID_fr_lua_scripts/uarOTA.lua
@@ -4,7 +4,7 @@ end
 
 local function run()
   -- Called periodically while the Special Function switch is on
---{"RTC":1693769182, "lat":435128245, "lon":14970095, "GSpd":0, "Hdg":193.33999633789, "GAlt":183.60000610352}
+--{"OTA":1, "lat":435128245, "lon":14970095, "GSpd":0, "Hdg":193.33999633789, "GAlt":183.60000610352}
 	serialSetPower(1, 1)
 	serialWrite('{"OTA": 1}\n')
 end
