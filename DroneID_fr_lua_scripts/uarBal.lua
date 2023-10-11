77eb10daa4dfdd7d992948aa9282fca7b0da5f23
freedom2000 <31324055+f2knpw@users.noreply.github.com>
2023-10-11 21:48:05 +0200

    Update uarBal.lua
    fixed a bug in EdgeTx (Separate lat long sensors merged to single GPS sensor by @frankiearzu in #4080 : see EdgeTx v2.9.2 : https://github.com/EdgeTX/edgetx/releases/tag/v2.9.1)

--- DroneID_fr_lua_scripts/uarBal.lua
+++ DroneID_fr_lua_scripts/uarBal.lua
@@ -1,18 +1,26 @@
 local function init()
   -- Called once when the script is loaded
-	mlat =  getFieldInfo("lat").id    -- here we get the numerical id of the filed "lat"
-	mlon =  getFieldInfo("lon").id    
-	mGSpd =  getFieldInfo("GSpd").id    
-	mHdg =  getFieldInfo("Hdg").id 
-	mGAlt =  getFieldInfo("GAlt").id 	
+	mGPS =  getFieldInfo("GPS").id    -- id of the field "GPS". GPS is a "table" with GPS.lat and GPS.lon  
+	mGSpd =  getFieldInfo("GSpd").id  -- id of the field "GSpd"
+	mHdg =  getFieldInfo("Hdg").id    -- id of the field "Hdg"
+	mGAlt =  getFieldInfo("GAlt").id  -- id of the field "GAlt"
 end
 
 local function run()
   -- Called periodically while the Special Function switch is on
---{"OTA":0, "lat":435128245, "lon":14970095, "GSpd":0, "Hdg":193.33999633789, "GAlt":183.60000610352}
+--{"OTA":0, "lat":43.5128245, "lon":1.4970095, "GSpd":0, "Hdg":193.33999633789, "GAlt":183.60000610352}
 	serialSetPower(1, 1) -- setPower port AUX2 on --> serialSetPower(port_nr, value)
-	serialWrite('{"OTA": 0, "lat":'..getValue(mlat)..', "lon":'..getValue(mlon)..', "GSpd":'..getValue(mGSpd)..', "Hdg":'..getValue(mHdg)..', "GAlt":'..getValue(mGAlt)..'}\n')
+    serialWrite('{"OTA": 0, "lat":'..(getValue(mGPS).lat)..', "lon":'..(getValue(mGPS).lon)..', "GSpd":'..getValue(mGSpd)..', "Hdg":'..getValue(mHdg)..', "GAlt":'..getValue(mGAlt)..'}\n')
 end
+
+
+local function background()
+  -- Called periodically while the Special Function switch is off
+end
+
+return { run=run, background=background, init=init }
+
+
 
 
 local function background()
